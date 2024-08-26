6f75b4401c8faa7c1fe62d866c50a3dd1c10f747
sanskar22082002 <sanskarkashyap156@gmail.com>
2024-08-26 11:52:01 +0530

    Optimize way

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -52,8 +52,8 @@ const router = createBrowserRouter(
   )
 
 )
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+React.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
