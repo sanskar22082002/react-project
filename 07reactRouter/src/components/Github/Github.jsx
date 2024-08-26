7362e10e792d7126ac4cc19fbce026c38bc6b823
sanskar22082002 <sanskarkashyap156@gmail.com>
2024-08-26 11:44:06 +0530

    Optimize way for fetching api

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react'
-
+import { useLoaderData } from 'react-router-dom'
 function Github() {
-    const [data, setdata] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/sanskar22082002')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            setdata(data)
-        })
+    const data = useLoaderData()
+    // const [data, setdata] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/sanskar22082002')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         setdata(data)
+    //     })
 
-    },[])
+    // },[])
     return (
         <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
         <img src={data.avatar_url} alt="Git picture" width={300} /></div>
@@ -18,3 +19,8 @@ function Github() {
     )
 }
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch ('https://api.github.com/users/sanskar22082002')
+    return response.json()
+}
